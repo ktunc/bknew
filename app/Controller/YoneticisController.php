de29d1c7922b341378c7dd899fb5815298fc6fc2
ktunc <kaatunc@gmail.com>
2017-07-21 08:37:33 +0300

    test

---
+++ app/Controller/YoneticisController.php
@@ -0,0 +1,15 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Yoneticis Controller
+ */
+class YoneticisController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
